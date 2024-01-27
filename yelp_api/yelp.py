ffc4fc4e9d96659bb487591ee99df0c402b9885d
liza <eshchehl@uci.edu>
2024-01-27 13:59:45 -0800

    finished the functions

--- yelp_api/yelp.py
+++ yelp_api/yelp.py
@@ -2,18 +2,15 @@ import json
 import urllib.request
 import urllib
 
-
 from urllib.error import HTTPError
 from urllib.parse import quote
-from urllib.parse import urlencode
 
 
 API_KEY = 'gbTLNjT_DtIGYldTPiy8mblU4Kv6Rk_ZKSsPjAg0_Cxr_7MgR2HJgdzoBbVEjjzSHDV5EN9JacV4UgRbZosTgbshm6mdIplQdpa6Iy5-CYBJC5wvpkMjYLuxVaa0ZXYx'
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 BUSINESS_PATH = '/v3/businesses/'
-DELIVERY_PATH = '/v3transactions/delivery/search'
-
+DELIVERY_PATH = '/v3/transactions/delivery/search'
 HEADERS = {'Authorization': f'Bearer {API_KEY}', 'Accept': 'application/json'}
 
 
@@ -21,8 +18,8 @@ def build_search_query_basic(location: str, type_of_food: str, category: str = N
                              price: int = None, sort_by: str = None,
                              limit: int = 2) -> dict:
     query = {
-        'term': type_of_food,
         'location': location,
+        'term': type_of_food,
     }
 
     if category is not None:
@@ -36,12 +33,11 @@ def build_search_query_basic(location: str, type_of_food: str, category: str = N
 
     return query
 
-
 def build_search_query_delivery(location: str, type_of_food: str, /, category: str = None,
                                 price: int = None) -> dict:
     query = {
-        'term': type_of_food,
-        'location': location
+        'location': location,
+        'term': type_of_food
     }
 
     if category is not None:
@@ -50,7 +46,6 @@ def build_search_query_delivery(location: str, type_of_food: str, /, category: s
         query['price'] = price
 
     return query
-
 
 def build_reviews_query(business_id: str, limit: int = 2, sort_by: str = 'yelp sort') -> dict:
     """Build a query for fetching Yelp reviews for a given business."""
@@ -62,7 +57,7 @@ def build_reviews_query(business_id: str, limit: int = 2, sort_by: str = 'yelp s
     return query
 
 
-def build_search_url(search_query: dict, parameter: str = BUSINESS_PATH,
+def build_search_url(search_query: dict, parameter: str = SEARCH_PATH,
                      reviews: bool = False) -> str:
     """creates a search url given the wanted parameters i.e term,location,catagory,price"""
     base_url = f'{API_HOST}{parameter}'
@@ -71,7 +66,6 @@ def build_search_url(search_query: dict, parameter: str = BUSINESS_PATH,
     else:
         url = f"{base_url}?{urllib.parse.urlencode(search_query)}"
     return url
-
 
 def build_reviews_url(business_id: str, limit: int = 2, sort_by: str = 'yelp_sort') -> str:
     """Create a URL for fetching Yelp reviews based on the provided parameters."""
@@ -82,7 +76,6 @@ def build_reviews_url(business_id: str, limit: int = 2, sort_by: str = 'yelp_sor
     }
     url = f"{base_url}?{urllib.parse.urlencode(url_params)}"
     return url
-
 
 def request(url: str, headers):
     """
@@ -104,15 +97,118 @@ def request(url: str, headers):
         return None
 
 
-if __name__ == "__main__":
-    # Example business_id
-    business_id = 'vu6PlPyKptsT6oEq50qOzA'
-    # Build the Yelp reviews query
-    reviews_query = build_reviews_query(business_id, limit = 1, sort_by = 'yelp_sort')
-    # Build the Yelp reviews URL
+def format_reviews_data(reviews_data: dict) -> str:
+    """
+    Format Yelp reviews data into a string for easy reading.
+    """
+    total_reviews = reviews_data.get('total', 0)
+    formatted_reviews = [f"Total: {total_reviews} reviews"]
+
+    for review in reviews_data.get('reviews', []):
+        user_name = review['user'].get('name', 'Unknown User')
+        rating = review.get('rating', 'N/A')
+        text = review.get('text', 'No review text available')
+
+        formatted_review = f"\nUser: {user_name}\nRating: {rating}\nReview: {text}"
+        formatted_reviews.append(formatted_review)
+
+    return ''.join(formatted_reviews)
+
+
+def format_business_data(business_data: dict) -> str:
+    ''''
+    Format Yelp business data into a string for easy reading.
+    '''
+
+    formatted_businesses = []
+
+    for business in business_data.get('businesses'):
+        name = business.get('name', 'Unknown business')
+        image = business.get('image_url', 'No image available')
+        website = business.get('url', 'No website available')
+        rating = business.get('rating', 'N/A')
+        review_count = business.get('review_count', 0)
+        categories = ', '.join(category['title'] for category in business.get('categories', []))
+        price = business.get('price', 'N/A')
+        address = ', '.join(business.get('location', {}).get('display_address', []))
+        phone = business.get('display_phone', 'N/A')
+
+        formatted_business = (
+            f"\nName: {name}! {rating} stars, {review_count} reviews\nPrice: {price} | Categories: {categories}\n"
+            f"Website: {website}\nImage URL: {image}\n"
+            f"Located at {address} | Call {phone}\n"
+        )
+        formatted_businesses.append(formatted_business)
+
+    return ''.join(formatted_businesses)
+
+
+def format_delivery_data(delivery_data: dict) -> str:
+    formatted_delivery_data = []
+
+    total_deliveries = delivery_data.get('total', 0)
+    formatted_delivery_data.append(f"Total Deliveries: {total_deliveries}")
+
+    for delivery in delivery_data.get('businesses', []):
+        name = delivery.get('name', 'Unknown Business')
+        rating = delivery.get('rating', 'N/A')
+        review_count = delivery.get('review_count', 0)
+        image_url = delivery.get('image_url', 'No image available')
+        url = delivery.get('url', 'No website available')
+        categories = ', '.join(category['title'] for category in delivery.get('categories', []))
+
+        formatted_delivery = (
+            f"\nName: {name}! {rating} stars, {review_count} reviews\n"
+            f"Categories: {categories}\n"
+            f"Website: {url}\nImage URL: {image_url}\n"
+        )
+
+        formatted_delivery_data.append(formatted_delivery)
+
+    return ''.join(formatted_delivery_data)
+
+
+def grab_business_id(business_data: dict, desired_name: str) -> str:
+    for business in business_data.get('businesses', []):
+        if desired_name in business.get('name', ''):
+            return business.get('id')
+
+    return None
+
+
+def find_and_print_restaurant_details(search_query: dict):
+    business_search_query = build_search_query_basic(**search_query)
+    business_search_url = build_search_url(business_search_query)
+
+    business_search_response = request(business_search_url, HEADERS)
+
+    if business_search_response:
+        print(format_business_data(business_search_response))
+    else:
+        print("No data found! Error occured, please retry.")
+
+
+def find_and_print_delivery_details(search_query: dict):
+    delivery_query = build_search_query_basic(**search_query)
+    delivery_url = build_search_url(delivery_query, DELIVERY_PATH)
+    delivery_data = request(delivery_url, HEADERS)
+
+    if delivery_data:
+        print(format_delivery_data(delivery_data))
+    else:
+        print("Error occured, please retry.")
+
+
+def find_and_print_rating_details(search_query: dict):
+    reviews_query = build_reviews_query(**search_query)
     reviews_url = build_reviews_url(**reviews_query)
-    # Make Yelp API request
+
     response_data = request(reviews_url, HEADERS)
-    # Process the response data as needed
+
     if response_data:
-        print(json.dumps(response_data, indent = 2))
+        print(format_reviews_data(response_data))
+    else:
+        print('None! Sorry something went wrong')
+
+if __name__ == "__main__":
+    print('hi')
