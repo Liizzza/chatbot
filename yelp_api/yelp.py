ca7d0ee9bd5163eda25d9fa1bae0b91afee95fd9
liza <eshchehl@uci.edu>
2024-01-26 23:08:13 -0800

    made search url for yelp api

--- yelp_api/yelp.py
+++ yelp_api/yelp.py
@@ -5,6 +5,7 @@ import pprint
 import sys
 import urllib
 
+
 from urllib.error import HTTPError
 from urllib.parse import quote
 from urllib.parse import urlencode
@@ -14,3 +15,12 @@ API_KEY = 'gbTLNjT_DtIGYldTPiy8mblU4Kv6Rk_ZKSsPjAg0_Cxr_7MgR2HJgdzoBbVEjjzSHDV5E
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 BUSINESS_PATH = '/v3/businesses/'
+
+
+def build_search_url(search_query):
+    base_url = f'{API_HOST}{SEARCH_PATH}'
+    headers = {'Authorization': f'Bearer {API_KEY}', 'Accept': 'application/json'}
+
+    url = f"{base_url}?/{headers}{urllib.parse.urlencode(search_query)}"
+
+    return url
