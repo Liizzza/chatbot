85cf577c4cef1b0bcf6164443c04f5a2e89f6e40
sabrinapark <sabrinyp@uci.edu>
2024-01-27 22:18:38 -0800

    added chatbot function.

--- skeleton.py
+++ skeleton.py
@@ -3,7 +3,9 @@ from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 import nltk
 import ssl
 import spacy
-
+import yelp
+import collections
+collections.Hashable = collections.abc.Hashable
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -12,89 +14,185 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-# nltk.download()
+food_chatbot = ChatBot('FoodBot', logic_adapters=[
+        "chatterbot.logic.BestMatch"
+    ])
 
-food_chatbot = ChatBot('FoodBot',
-                       logic_adapters=[
-                           {"import_path": "chatterbot.logic.BestMatch"
-                            "statement_comparison_function": }
-                       ])
+trainer = ChatterBotCorpusTrainer(food_chatbot)
+datasets_to_train = ['chatterbot.corpus.english.food',
+                    'chatterbot.corpus.english.conversations',
+                    'chatterbot.corpus.english.greetings',
+                    'chatterbot.corpus.english.health'
+                     ]
 
-# Create a new trainer for the chatbot
-trainer = ListTrainer(chatbot=food_chatbot)
+# 'chatterbot.corpus.english.greetings',
+#                      'chatterbot.corpus.english.conversations',
+#                      'chatterbot.corpus.english.food',
+#                      'chatterbot.corpus.english.health'
+
+trainer.train(*datasets_to_train)
+
+trainer_list = ListTrainer(food_chatbot)
+
+trainer_list.train([
+    "Hi", "Hello, I am your FoodBot🍔! How can I help you",
+    "I am hungry.", "Here are some suggestions: pizza places, burger places, and italian places",
+    "Recommend me healthy food.", "There are many places with healthy food! To learn more type 'where should i eat'",
+    "Thank you for your help", "No problem. Anything else you need?"
+])
+# trainer_list.train([
+#     "I don't know what to eat", "Are you really hungry?",
+#     "Yes I am", "Oh no, would you like me to recommend you places?",
+#     "Yes I want suggestions", "Here are some suggestions: pizza places, burger places, and italian places",
+#     "I want ones in my area", "For more recommendations ask 'where should I eat'"
+#     "Thank you for your help.", "No problem. Anything else you need?"
+#     "i am hungry", "excellent! i can recommend you food!"
+#     ]
+# )
+trainer_list.train([
+    "Hello! I am hungry", "Would you like recommendations?",
+    "I am hungry", "Hi hungry, Im dad. Joking. Would you look food recs?",
+    "I want to be healthy", "Perfect! I can help start your search for health restaurants!",
+    "I want to be fat", "Im sure we have our own wants. But, I can help recommend healthy alternatives",
+    "Hello", "Hello, what can I help you with?"
+    "I want help with finding a restaurant", "Of course! please enter what you want to search for.",
+    "What's your all-time favorite food", "Speaking of favorite dishes, have you ever tried cooking that special dish at home, and if so, what's your secret ingredient or technique to make it truly exceptional?"
+])
+trainer_list.train([
+    'im hungry', 'Let me help find you a healthy place recommendation!',
+    'im hungry', 'Okay! type "search for food in my area"',
+    'im hungry', 'Let me recommend you fresh food in you area! Just ask me to search for something'
+    'Im hungry', 'Let me find you fresh, healthy food, just ask me to search!',
+    'I want fast food', 'Fast food is cheap and easy but unhealthy. Let me recommend you healthier options',
+    'i want fast food', 'Fast food is cheap and easy but unhealthy. Let me recommend you healthier options'
+])
+trainer_list.train([
+    'Recommend me food', "Of course! Tell me to help find food in your area",
+    "recommend me food", "Of course! Tell me to help"
+])
+
+trainer_list.train([
+    "Recommend me a boba place", "Of course! Tell me to find a boba place in your area",
+    "I want to drink boba", "I will help you find boba in your area, please type 'find boba in my area'",
+    "I want coffee", "Of course! I can help you find coffee in your area, please enter 'find coffee in my area'"
+    "boba recommendations", "Okay I can help you find some as soon as you specify to find boba in an area",
+    "coffee recommendations", "Okay I can help you find some as soon as you specify to find coffee in an area",
+    "matcha reccommendations", "Okay! I can help you find some as soon as you specify to find matcha in your area",
+    "matcha", "Do you mean, the best thing on earth? I can help you find the universe's gift to the world with an easy search"
+])
+#
+# trainer_list.train([
+#     'hi i want food recommendations', 'Of course! I am here to help, what food recommendations do u want.'
+#     'I would like you to search for restaurants', "Okay there you go"
+#     ]
+#     )
+# trainer_list.train([
+#     'I am not happy with anything you sent me,', "Oh no im so sorry! Would you like to search again?"
+#     "Yes, please search for new food", "Okay here!"
+# ])
+# trainer_list.train([
+#     'Hello, I want food recommendations', "Okay would you like me to search for restaurants?",
+#     "im hungry", "I can help with that! What would u like."
+# ])
 
 
-#Train the chatbot on a custom dataset for food-related queries
-custom_food_dataset = [
-    'What should I eat?',
-    'burgers'
-]
 
-#all_data = ['chatterbot.corpus.english']
-#all_data.extend(custom_food_dataset)
-
-# Train the chatbot on the custom food dataset
-trainer.train("chatterbot.corpus.english")
-
-def get_restaurant_recommendation(location, cuisine):
-    pass
-
-def book_reservation(restaurant_name):
-    pass
-
-
-def get_restaurant_recommendation(location, cuisine):
-    pass
-
-def has_city_name(response):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(response)
-    return any(ent.label_ == "GPE" for ent in doc.ents)
-
-def extract_city_name(response):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(response)
-    city_names = [ent.text for ent in doc.ents if ent.label_ == "GPE"]
-    return city_names
-
+agreeing_words = ["YES",'OKAY',"SURE","OK","Y","OKY"]
 def chat_with_bot():
-    print("Food Bot: Hello! I'm FoodBot. Ask me anything about food.")
+    print("Food Bot🍔: Hello! I'm FoodBot. Ask me anything about food.")
+    category = None
+    location = None
+    price = None
+    type_of_food = None
+    price = None
+    limit = 4
+    cuisine = None
 
     while True:
         user_input = input("You: ")
-        location = None
-        city = None
 
-        if 'where' in user_input:
-            if location == None:
-                input_city = input('What city would you like')
-                # logic to implement yelp API
-            elif city == None:
-                input_cuisine = input('What are you searching for')
-                # logic to implement yelp API
-
+        if 'WHERE' in user_input.upper() or 'SEARCH' in user_input.upper() or 'FIND' in user_input.upper():
+            location = input('Food Bot🍔: Enter your location\nYou: ')
+            if cuisine is None:
+                type_of_food = input('Food Bot🍔: What are you searching for\nYou: ')
+            answer = input("Food Bot🍔: Would you like any other criteria?\nYou: ")
+            if answer.upper() in agreeing_words:
+                answer = input(f"Food Bot🍔:Would you like a specific type of {type_of_food}?\nYou: ")
+                if answer.upper() in agreeing_words:
+                    category = input("Food Bot🍔: Okay what is it?\nYou: ")
+                answer = input(f"Food Bot🍔: Would you like a specific price?\nYou: ")
+                if answer.upper() in agreeing_words:
+                    price = input("Food Bot🍔: Enter 1,2,3, or 4!\nYou: ")
+                    try:
+                        price = int(price)
+                    except:
+                        print("Food Bot🍔: Woops that wasnt a choice!")
+                answer = input("Food Bot🍔: Would you like a certain number of options?\nFood Bot: Must be less than twenty!\nYou: ")
+                if answer.upper() in agreeing_words:
+                    limit = input("Food Bot🍔: Great! Enter your amount\nYou: ")
+                    try:
+                        limit = int(limit)
+                        if limit > 20:
+                            print("Food Bot🍔: This was way too much.")
+                    except:
+                        print("Food Bot🍔: That was not an option!")
+            print("Food Bot🍔: Grabbing food data now!")
+            query = dict(location = location,
+                        type_of_food = type_of_food,
+                        category = category,
+                        price = price,
+                        sort_by = 'best_match',
+                        limit = limit
+                        )
+            yelp.find_and_print_restaurant_details(query)
+            answer = input("Food Bot🍔: Would you like to know more about any options?\nYou: ")
+            if answer.upper() in agreeing_words:
+                answer = input("Food Bot🍔: Great! Please enter the name of the place you want without the exclamation point!\nYou: ")
+                query = yelp.build_search_query_basic(location = location,
+                                                      type_of_food = type_of_food,
+                                                      category = category,
+                                                      price = price,
+                                                      sort_by = 'best_match',
+                                                      limit = limit
+                )
+                url = yelp.build_search_url(query)
+                response = yelp.request(url)
+                business_id = yelp.grab_business_id(response,answer)
+                if business_id is not None:
+                    reviews_query = yelp.build_reviews_query(business_id, limit=4)
+                try:
+                    print(yelp.find_and_print_rating_details(reviews_query))
+                except:
+                    print("Food Bot🍔: Woops something went wrong! Please try again later")
+        elif 'BYE' in user_input.upper():
+            print(f'Food Bot🍔: Goodbye! I hope I helped you find your new favorite place!')
+            break
         else:
-            location = getting_city(user_input)
-            cuisine = getting_cuisine(user_input)
-
-            # function to get_restaurant(location, cuisine)
             response = food_chatbot.get_response(user_input)
-            print(f'Food Bot: {response}')
+            print(f'Food Bot🍔: {response}')
+            #
+            # feedback = input("Was the response helpful? (yes/no): ")
+            # if feedback.lower() == 'yes':
+            #     # If the response was helpful, continue the conversation
+            #     new_input = input("You: ")
+            #     new_response = input("Bot: ")
 
-def checking_response(response, value_type='city'):
-    if value_type not in ['city', 'cuisine']:
-        raise ValueError("Invalid value_type. Use 'city' or 'cuisine'.")
-    if value_type == 'city':
-        return getting_city(response)
-    elif value_type == 'cuisine':
-        return getting_cuisine(response)
+            #     # Train the chatbot with the new interaction
+            #     trainer_list.train([
+            #         user_input,
+            #         response.text,
+            #         new_input,
+            #         new_response
+            #     ])
+            # else:
+            #     user_desired = input("What is your desired response?: ")
+            #     trainer_list.train([
+            #         user_input,
+            #         user_desired
+            #     ])
 
-def getting_city(response):
-    if has_city_name(response):
-        city_name = extract_city_name(response)
-        return city_name
-    else:
-        return None
+
+
 
 def getting_cuisine(response):
     if 'breakfast' in response:
---
+++ yelp.py
@@ -0,0 +1,214 @@
+import json
+import urllib.request
+import urllib
+
+from urllib.error import HTTPError
+from urllib.parse import quote
+
+
+API_KEY = 'gbTLNjT_DtIGYldTPiy8mblU4Kv6Rk_ZKSsPjAg0_Cxr_7MgR2HJgdzoBbVEjjzSHDV5EN9JacV4UgRbZosTgbshm6mdIplQdpa6Iy5-CYBJC5wvpkMjYLuxVaa0ZXYx'
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'
+DELIVERY_PATH = '/v3/transactions/delivery/search'
+HEADERS = {'Authorization': f'Bearer {API_KEY}', 'Accept': 'application/json'}
+
+
+def build_search_query_basic(location: str, type_of_food: str, category: str = None,
+                             price: int = None, sort_by: str = 'best_match',
+                             limit: int = 2) -> dict:
+    query = {
+        'location': location,
+        'term': type_of_food,
+    }
+
+    if category is not None:
+        query['category'] = category
+    if price is not None:
+        query['price'] = price
+    if sort_by is not None:
+        query['sort_by'] = sort_by
+    if limit is not None:
+        query['limit'] = limit
+
+    return query
+
+def build_search_query_delivery(location: str, type_of_food: str, /, category: str = None,
+                                price: int = None) -> dict:
+    query = {
+        'location': location,
+        'term': type_of_food
+    }
+
+    if category is not None:
+        query['category'] = category
+    if price is not None:
+        query['price'] = price
+
+    return query
+
+def build_reviews_query(business_id: str, limit: int = 2, sort_by: str = 'yelp sort') -> dict:
+    """Build a query for fetching Yelp reviews for a given business."""
+    query = {
+        'business_id': business_id,
+        'limit': limit,
+        'sort_by': sort_by
+    }
+    return query
+
+
+def build_search_url(search_query: dict, parameter: str = SEARCH_PATH,
+                     reviews: bool = False) -> str:
+    """creates a search url given the wanted parameters i.e term,location,catagory,price"""
+    base_url = f'{API_HOST}{parameter}'
+    if parameter == BUSINESS_PATH and reviews is True:
+        url = f"{base_url}?"
+    else:
+        url = f"{base_url}?{urllib.parse.urlencode(search_query)}"
+    return url
+
+def build_reviews_url(business_id: str, limit: int = 2, sort_by: str = 'yelp_sort') -> str:
+    """Create a URL for fetching Yelp reviews based on the provided parameters."""
+    base_url = f'{API_HOST}{BUSINESS_PATH}{business_id}/reviews'
+    url_params = {
+        'limit': limit,
+        'sort_by': sort_by
+    }
+    url = f"{base_url}?{urllib.parse.urlencode(url_params)}"
+    return url
+
+def request(url: str, headers = HEADERS):
+    """
+    sends a url request based off url and headers
+    """
+
+    try:
+        req = urllib.request.Request(url, headers = headers)
+        with urllib.request.urlopen(req) as response:
+            data = json.loads(response.read().decode('utf-8'))
+            return data
+
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error {e.code}: {e.reason}")
+        return None
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def format_reviews_data(reviews_data: dict) -> str:
+    """
+    Format Yelp reviews data into a string for easy reading.
+    """
+    total_reviews = reviews_data.get('total', 0)
+    formatted_reviews = [f"Total: {total_reviews} reviews"]
+
+    for review in reviews_data.get('reviews', []):
+        user_name = review['user'].get('name', 'Unknown User')
+        rating = review.get('rating', 'N/A')
+        text = review.get('text', 'No review text available')
+
+        formatted_review = f"\nUser: {user_name}\nRating: {rating}\nReview: {text}"
+        formatted_reviews.append(formatted_review)
+
+    return ''.join(formatted_reviews)
+
+
+def format_business_data(business_data: dict) -> str:
+    ''''
+    Format Yelp business data into a string for easy reading.
+    '''
+
+    formatted_businesses = []
+
+    for business in business_data.get('businesses'):
+        name = business.get('name', 'Unknown business')
+        image = business.get('image_url', 'No image available')
+        website = business.get('url', 'No website available')
+        rating = business.get('rating', 'N/A')
+        review_count = business.get('review_count', 0)
+        categories = ', '.join(category['title'] for category in business.get('categories', []))
+        price = business.get('price', 'N/A')
+        address = ', '.join(business.get('location', {}).get('display_address', []))
+        phone = business.get('display_phone', 'N/A')
+
+        formatted_business = (
+            f"\nName: {name}! {rating} stars, {review_count} reviews\nPrice: {price} | Categories: {categories}\n"
+            f"Website: {website}\nImage URL: {image}\n"
+            f"Located at {address} | Call {phone}\n"
+        )
+        formatted_businesses.append(formatted_business)
+
+    return ''.join(formatted_businesses)
+
+
+def format_delivery_data(delivery_data: dict) -> str:
+    formatted_delivery_data = []
+
+    total_deliveries = delivery_data.get('total', 0)
+    formatted_delivery_data.append(f"Total Deliveries: {total_deliveries}")
+
+    for delivery in delivery_data.get('businesses', []):
+        name = delivery.get('name', 'Unknown Business')
+        rating = delivery.get('rating', 'N/A')
+        review_count = delivery.get('review_count', 0)
+        image_url = delivery.get('image_url', 'No image available')
+        url = delivery.get('url', 'No website available')
+        categories = ', '.join(category['title'] for category in delivery.get('categories', []))
+
+        formatted_delivery = (
+            f"\nName: {name}! {rating} stars, {review_count} reviews\n"
+            f"Categories: {categories}\n"
+            f"Website: {url}\nImage URL: {image_url}\n"
+        )
+
+        formatted_delivery_data.append(formatted_delivery)
+
+    return ''.join(formatted_delivery_data)
+
+
+def grab_business_id(business_data: dict, desired_name: str) -> str:
+    for business in business_data.get('businesses', []):
+        if desired_name in business.get('name', ''):
+            return business.get('id')
+
+    return None
+
+
+def find_and_print_restaurant_details(search_query: dict):
+    business_search_query = build_search_query_basic(**search_query)
+    business_search_url = build_search_url(business_search_query)
+
+    business_search_response = request(business_search_url, HEADERS)
+
+    if business_search_response:
+        print(format_business_data(business_search_response))
+    else:
+        print("No data found! Error occured, please retry.")
+
+
+def find_and_print_delivery_details(search_query: dict):
+    delivery_query = build_search_query_basic(**search_query)
+    delivery_url = build_search_url(delivery_query, DELIVERY_PATH)
+    delivery_data = request(delivery_url, HEADERS)
+
+    if delivery_data:
+        print(format_delivery_data(delivery_data))
+    else:
+        print("Error occured, please retry.")
+
+
+def find_and_print_rating_details(search_query: dict):
+    reviews_query = build_reviews_query(**search_query)
+    reviews_url = build_reviews_url(**reviews_query)
+
+    response_data = request(reviews_url, HEADERS)
+
+    if response_data:
+        print(format_reviews_data(response_data))
+    else:
+        print('None! Sorry something went wrong')
+
+if __name__ == "__main__":
+    pass
